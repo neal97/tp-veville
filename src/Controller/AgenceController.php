3d1d45ee3dccbb0fc3c482085c802f10c2e8793a
Adil BOUAJAJ <adilbouaja@gmail.com>
2023-02-13 15:48:05 +0100

    add agence

--- src/Controller/AgenceController.php
+++ src/Controller/AgenceController.php
@@ -2,17 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\Agences;
+use App\Form\AgenceType;
+use App\Repository\AgencesRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AgenceController extends AbstractController
 {
     #[Route('/agence', name: 'app_agence')]
-    public function index(): Response
+    public function index(AgencesRepository $repoAgences, Request $request, EntityManagerInterface $manager): Response
     {
+         $agences = $repoAgences->findAll();
+                     
+         $agence = new Agences;
+         $form = $this->createForm(AgenceType::class, $agence );
+
+         $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $manager->persist($agence);
+            $manager->flush(); 
+        }
+
         return $this->render('agence/index.html.twig', [
             'controller_name' => 'AgenceController',
-        ]); 
+            'agences' => $agences,
+            'formAgence' => $form->createView()
+                ]); 
     }
 }
--- src/Form/AgenceType.php
+++ src/Form/AgenceType.php
@@ -4,7 +4,9 @@ namespace App\Form;
 
 use App\Entity\Agences;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,28 +15,56 @@ class AgenceType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('titre', TextreaType::class, [
+            ->add('titre', TextType::class, [
                 "label" => "titre ",
-                "require" => false,
+                "required" => false,
+                "attr" => [
+                    "placeholder"=> "titre",
+                    "class"=> "form-control",
+                ]
             ]) 
-            ->add('adresse', TextreaType::class, [
+            ->add('adresse', TextType::class, [
                 "label" => "adresse de l'agence",
-                "require" => false,
+                "required" => false,
+                "attr" => [
+                    "placeholder"=> "adresse",
+                    "class"=> "form-control",
+                ]
             ])
-            ->add('ville', TextreaType::class, [
+            ->add('ville', TextType::class, [
                 "label" => "ville de l'agence",
-                "require" => false,
+                "required" => false,
+                "attr" => [
+                    "placeholder"=> "ville",
+                    "class"=> "form-control",
+                ]
             ])
-            ->add('cp', TextreaType::class, [
+            ->add('cp', TextType::class, [
                 "label" => "cp de la ville",
-                "require" => false,
+                "required" => false,
+                "attr" => [
+                    "placeholder"=> "cp",
+                    "class"=> "form-control",
+                ]
             ])
-            ->add('description', TextreaType::class, [
+            ->add('description', TextareaType::class, [
                 "label" => "description de l'agence",
-                "require" => false,
+                "required" => false,
+                "attr" => [
+                    "placeholder"=> "description",
+                    "class"=> "form-control",
+                ]
             ])
-            ->add('photo')
-        ;
+            ->add('photo', TextType::class, [
+                "label" => "photo de l'agence",
+                "required" => false,
+                "attr" => [
+                    "placefolder" => "photo",
+                    "class" => "form-control"
+                ] 
+            ]) 
+        
+        ->add('Ajouter', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
