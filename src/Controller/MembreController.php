6bd39999c995f88c44777dee5eeb8aad35183f98
Neal Belfan <neal973@live.fr>
2023-02-13 21:38:00 +0100

    push membre

--- src/Controller/MembreController.php
+++ src/Controller/MembreController.php
@@ -13,17 +13,16 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MembreController extends AbstractController
 {
-    
-    #[Route('/membres', name: 'membres')]
+    #[Route('/test', name: 'app_membre')]
     public function index(MembreRepository $repoMembre,Request $request, EntityManagerInterface $manager,Membre $membre): Response
     {
-        // $membres = $repoMembre->findAll();
+        $membres = $repoMembre->findAll();
 
-        // $membre = new Membre;
+        $membre = new Membre;
 
-        // $form = $this->createForm(MembreType::class , $membre);
+        $form = $this->createForm(MembreType::class , $membre);
  
-        // $form->handleRequest($request);
+        $form->handleRequest($request);
  
         // if($form->isSubmitted() && $form->isValid())
         // {       
@@ -37,28 +36,59 @@ class MembreController extends AbstractController
         //      $manager->flush();
 
  
-        //      return $this->redirectToRoute("membres");
+        //      return $this->redirectToRoute("app_membre");
         // } 
+
+        // $id = $membre->getId();
+        // $manager->remove($membre);
+        // $manager->flush();
+        // $this->addFlash('succes',' Le Membre a bien été supprimer');
  
 
         return $this->render('membre/index.html.twig', [
             'controller_name' => 'MembreController',
-            // 'membres' => $membres ,
-            // 'FormMembre' => $form->createView()
+            'membres' => $membres ,
+            'FormMembre' => $form->createView()
         ]);
     }
 
+    #[Route('/membre/update/{id}', name: 'update_membre')]
+    public function update(Membre $membre, Request $request,  EntityManagerInterface $manager)
+    {
+       $form = $this->createForm(MembreType::class, $membre);
+       $form->handleRequest($request);
+
+       if($form->isSubmitted() && $form->isValid())
+       {       
+            $manager->persist($membre);
+            $manager->flush();
+
+            $this->addFlash('succes',' Le Membre' . $membre->getId() . 'a bien été modifier');
+
+            return $this->redirectToRoute("app_membre");
+       } 
+
+        return $this->render('membre/update.html.twig', [
+                "formMembre" => $form->createView(),
+                'membre' => $membre
+             ]);
+    }
 
 
-    #[Route('/membre/delete/{id}', name: 'delete')]
+    #[Route('/membre/delete/{id}', name: 'delete_membre')]
     public function delete(Membre $membre, EntityManagerInterface $manager)
     {
+      
+
+       $id = $membre->getId();
+
+       $manager->remove($membre);
+       $manager->flush();
+
+       $this->addFlash("Succes","le membre N°". $id . "a bien été supprimer");
+       return $this->redirectToRoute("app_membre");
      
-        $id = $membre->getId();
-        $manager->remove($membre);
-        $manager->flush();
-        $this->addFlash('succes',' Le Membre a bien été supprimer');
-        return $this->redirectToRoute("membres");
-    }
+
+}
 
 }
